ab7479e4a5548b84ea7438e54952683ed95cf57f
Afonso Araújo <afonso16araujo@gmail.com>
2023-04-29 19:40:28 +0100

    new feature/add famyli photo

--- model/specialdonation/load_familys.php
+++ model/specialdonation/load_familys.php
@@ -1,15 +1,16 @@
 <?php 
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+
 require '/wamp64/www/STR/configurations/dbconnection.php';
 $sql = "SELECT * FROM familias_doacaoespecial";
 $result = $conn->query($sql);
-$count = 0;
 if ($result->num_rows > 0) {
     while ($row = mysqli_fetch_array($result)) {
+        $foto_familia = base64_encode($row['foto_familia']);
         echo '
         <div class="blog-slider__item swiper-slide">
             <div class="blog-slider__img">
-                <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="" />
+                <img src="data:image/*;base64,'.$foto_familia.'">
             </div>
             <div class="blog-slider__content">
                 <span class="blog-slider__code">'.$row['data_chegada'].'</span>
