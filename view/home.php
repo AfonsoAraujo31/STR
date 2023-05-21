b96f3297c8b3ce3d9b3ff420c16520ebb6f56af0
Afonso Araújo <afonso16araujo@gmail.com>
2023-05-21 23:13:07 +0100

    animated icon adicionado

--- view/home.php
+++ view/home.php
@@ -36,7 +36,7 @@
     <div id="includedLogoContent"></div>
     <!-- HEADER *___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*___*-->
     <ul class="nav_links bg-strong-gray" id="routesList">
-      <li class="li"><a class="active hover:text-white" href="#" id="homeLink"><i class="fa-solid fa-house"></i>Início</a></li>
+      <li class="li"><a class="active hover:text-white" href="../view/home.php"><i class="fa-solid fa-house"></i>Início</a></li>
       <li class="li"><a class="text-label-gray hover:text-white" href="../view/volunteer.php"><i class="fa-solid fa-handshake-angle"></i> Voluntariado</a></li>
       <li class="li"><a class="text-label-gray hover:text-white" href="../view/specialdonation.php"><i class="fa-solid fa-circle-dollar-to-slot"></i> Doação Especial </a></li>
       <li>
